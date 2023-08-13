da835f50bce5e0c2ef1b40ad5d0bbed79454dfb3
amirhosein24 <am.heidari24@gmail.com>
2023-08-13 15:06:48 +0330

    1

---
+++ json.py
@@ -0,0 +1,20 @@
+import json
+from json import loads
+from json import loads
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = loads(x)
+
+# the result is a Python dictionary:
+print(y)
+
+
+
+
+
+
+
+
+
